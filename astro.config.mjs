c06a212bf9c9e91ae170e4810aad636c137c9053
webmanufaktur <info@webmanufaktur.net>
2023-10-24 03:31:31 +0200

    WIP 2023-10-24

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import compressor from 'astro-compressor'
+import prefetch from '@astrojs/prefetch'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		viewTransitions: true,
+		assets: true,
+	},
+	site: 'http://localhost:3000',
+	twitter: 'html4seo',
+	integrations: [tailwind(), compressor(), prefetch(), sitemap()],
+})
